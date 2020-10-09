dd51551a8c9db2c8b2729aaf231a1a748fa75fa2
DoHoang <51627232+hoangdh62@users.noreply.github.com>
2020-10-09 10:22:39 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,39 +1,46 @@
 const PORT = process.env.PORT || 3000;
 const HOSTNAME = '0.0.0.0';
 
+// const express = require('express');
+// const path = require('path');
+// const cors = require('cors');
+
+// let app = express();
+
+// // Bypass CORS error
+// // var whitelist = ['https://gogul09.github.io/models/mobilenet/model.json']
+// var corsOptions = {
+//   origin: '*'
+// }
+
+// // Cấu hình root mặc định.
+// app.use(express.static(__dirname));
+
+// app.get("/hello", cors(corsOptions), (req, res, next) => {
+// 	// res.header("Access-Control-Allow-Origin", "https://0.0.0.0:5000"); // update to match the domain you will make the request from
+//   	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+// // 	res.sendFile(path.join(__dirname+"/index.html"));
+// 	res.send("hello");
+// // 	next();
+// });
+
+// app.listen(PORT, (err) => {
+// 	if (err) {
+// 		console.log(err);
+// 	} else {
+// 		console.log("Website is Up on PORT %d and HOSTNAME %s !", PORT, app.get('env'));
+// 	}
+// });
 const express = require('express');
-const path = require('path');
-const cors = require('cors');
-var multer = require('multer');
-var upload = multer();
+const multer = require('multer');
 
-let app = express();
+const app = express();
+const upload = multer();
 
-// Bypass CORS error
-// var whitelist = ['https://gogul09.github.io/models/mobilenet/model.json']
-var corsOptions = {
-  origin: '*'
-}
-
-// Cấu hình root mặc định.
-app.use(express.static(__dirname));
-
-app.post("/profile", upload.array(), function (req, res, next) {
+app.post('/profile', upload.array(), function (req, res, next) {
 	res.send("success")
 });
 
-app.get("/hello", cors(corsOptions), (req, res, next) => {
-	// res.header("Access-Control-Allow-Origin", "https://0.0.0.0:5000"); // update to match the domain you will make the request from
-  	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-// 	res.sendFile(path.join(__dirname+"/index.html"));
+app.get("/hello", function (req, res, next) => {
 	res.send("hello");
-// 	next();
-});
-
-app.listen(PORT, (err) => {
-	if (err) {
-		console.log(err);
-	} else {
-		console.log("Website is Up on PORT %d and HOSTNAME %s !", PORT, app.get('env'));
-	}
 });
