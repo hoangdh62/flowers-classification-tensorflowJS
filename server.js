7234ae522e69ba0b5e07314adaf686bf30ec675e
DoHoang <51627232+hoangdh62@users.noreply.github.com>
2020-10-09 09:33:21 +0700

    Update server.js

--- server.js
+++ server.js
@@ -17,18 +17,18 @@ var corsOptions = {
 // Cấu hình root mặc định.
 app.use(express.static(__dirname));
 
-// app.get("/", cors(corsOptions), (req, res, next) => {
-// 	// res.header("Access-Control-Allow-Origin", "https://0.0.0.0:5000"); // update to match the domain you will make the request from
-//   	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-// // 	res.sendFile(path.join(__dirname+"/index.html"));
-// 	res.send("hello");
-// // 	next();
-// });
+app.get("/hello", cors(corsOptions), (req, res, next) => {
+	// res.header("Access-Control-Allow-Origin", "https://0.0.0.0:5000"); // update to match the domain you will make the request from
+  	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+// 	res.sendFile(path.join(__dirname+"/index.html"));
+	res.send("hello");
+// 	next();
+});
 
 app.listen(PORT, (err) => {
 	if (err) {
 		console.log(err);
 	} else {
-		console.log("Website is Up on PORT %d and HOSTNAME %s !", PORT, app.get('env'));
+		console.log("Website is Up on PORT %d and HOSTNAME %s !", PORT, app.get('hello'));
 	}
 });
