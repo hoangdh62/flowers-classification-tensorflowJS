790b8ca47ead8f8b23020916939d79e495b96faa
DoHoang <51627232+hoangdh62@users.noreply.github.com>
2020-10-09 09:17:54 +0700

    Update server.js

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.get("/", cors(corsOptions), (req, res, next) => {
   	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 // 	res.sendFile(path.join(__dirname+"/index.html"));
 	res.send("hello");
-	next();
+// 	next();
 });
 
 app.listen(PORT, (err) => {
