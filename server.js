62b860ccbeffe96be492fa11f861dabbad70f208
DoHoang <51627232+hoangdh62@users.noreply.github.com>
2020-10-09 14:32:15 +0700

    Update server.js

--- server.js
+++ server.js
@@ -35,6 +35,8 @@ const HOSTNAME = '0.0.0.0';
 var express = require('express');
 var app = express();
 
+app.use(express.json());
+
 app.get('/', function (req, res) {
   res.send('Hello World!');
 })
